d6ce61cf20b7254e54e8b54b35d569522d0a0f13
Shiv Shankar S <shivshankarsajeev2000@gmail.com>
2025-04-21 20:48:19 +0530

    index.html created

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 from flask import Flask, render_template
 import pandas as pd 
 import numpy as np 
+
+app = Flask(__name__)
+
+@app.route("/")
+def home(): 
+    return render_template('index.html')
+
+
+
+if __name__ == "__main__": 
+    app.run(debug=True)
