72a72e9114ce04f120feb956cd5d6d06e6b3ed4c
Shiv Shankar S <shivshankarsajeev2000@gmail.com>
2025-04-21 21:03:12 +0530

    app.py created

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 from flask import Flask, render_template
 import pandas as pd 
 import numpy as np 
+import pickle 
 
 app = Flask(__name__)
 
+pipe = pickle .load(open('pipe.pkl','rb'))
+df = pickle.load(open('df.pkl','rb'))
+
 @app.route("/")
 def home(): 
-    return render_template('index.html')
+    return render_template('index.html', companies = df['Company'].unique(),
+                                         types = df['TypeName'].unique(), 
+                                          cpus = df['Cpu_brand'].unique(),
+                                          gpus = df['Gpu_brand'].unique(),
+                                            oss = df['os'].unique())
 
+@app.route('/predict', methods = ['POST'])
+def predict(): 
+    try: 
 
+    except:
 
 if __name__ == "__main__": 
     app.run(debug=True)
